034159c9d35308e4c51191d605bb5fac641eda68
demirtaserdem <demirtaserdem@gmail.com>
2020-03-17 10:52:42 +0300

    update production.py

--- ytadj/settings/production.py
+++ ytadj/settings/production.py
@@ -3,12 +3,12 @@ from ytadj.settings.base import *
 ALLOWED_HOSTS = ['app2.demirtas.biz']
 DEBUG = False
 
-DATABASE_ENGINE = config.get('database', 'DATABASE_ENGINE')
-DATABASE_NAME = config.get('database', 'DATABASE_NAME')
-DATABASE_USER = config.get('database', 'DATABASE_USER')
-DATABASE_PASSWORD = config.get('database', 'DATABASE_PASSWORD')
-DATABASE_HOST = config.get('database', 'DATABASE_HOST')
-DATABASE_PORT = config.get('database', 'DATABASE_PORT')
+DATABASES['default']['ENGINE'] = config.get('database', 'DATABASE_ENGINE')
+DATABASES['default']['NAME'] = config.get('database', 'DATABASE_NAME')
+DATABASES['default']['USER']= config.get('database', 'DATABASE_USER')
+DATABASES['default']['PASSWORD'] = config.get('database', 'DATABASE_PASSWORD')
+DATABASES['default']['HOST'] = config.get('database', 'DATABASE_HOST')
+DATABASES['default']['PORT'] = config.get('database', 'DATABASE_PORT')
 
 #ssl settings
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
